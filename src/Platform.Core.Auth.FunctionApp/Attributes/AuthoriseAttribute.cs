48edcdeadb5eaae2a6bb79bb6bd1e12edc9199ab
Derrick Sanya <Derrick Sanya@DESKTOP-TRJNK3F>
2024-03-21 12:57:28 +0000

    merge

---
+++ src/Platform.Core.Auth.FunctionApp/Attributes/AuthoriseAttribute.cs
@@ -0,0 +1,31 @@
+﻿namespace Platform.Core.Auth.FunctionApp.Attributes;
+
+/// <summary>
+/// Set at Function class or method level to
+/// set what scopes/user roles/app roles are required in requests.
+/// </summary>
+/// <remarks>
+/// If you do not specify app roles, calls without user context will fail.
+/// Same goes for scopes/user roles; calls with user context will fail if both are not specified.
+/// </remarks>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+public class AuthoriseAttribute : Attribute
+{
+    /// <summary>
+    /// Defines which scopes (aka delegated permissions) are accepted. In this sample these
+    /// must be combined with <see cref="UserRoles"/>.
+    /// </summary>
+    public string[] Scopes { get; set; } = [];
+
+    /// <summary>
+    /// Defines which user roles are accpeted.
+    /// Must be combined with <see cref="Scopes"/>.
+    /// </summary>
+    public string[] UserRoles { get; set; } = [];
+
+    /// <summary>
+    /// Defines which app roles (aka application permissions)
+    /// are accepted.
+    /// </summary>
+    public string[] AppRoles { get; set; } = [];
+}
---
+++ src/Platform.Core.Auth.FunctionApp/Configuration/AuthConfiguration.cs
@@ -0,0 +1,62 @@
+﻿using System.Globalization;
+
+namespace Platform.Core.Auth.FunctionApp.Configuration;
+
+/// <summary>
+/// Auth Configuration
+/// </summary>
+public class AuthConfiguration
+{
+    /// <summary>
+    /// OpenId Configuration Path
+    /// </summary>
+    private const string OpenIdConfigurationPath = ".well-known/openid-configuration";
+
+    /// <summary>
+    /// the Azure AD instance eg.https://login.microsoftonline.com/
+    /// </summary>
+    public string Instance { get; set; } = "https://login.microsoftonline.com/{0}/v2.0";
+
+    /// <summary>
+    /// The appication Id in the app registration
+    /// </summary>
+    public required string ClientId { get; set; }
+
+    /// <summary>
+    /// The Client Secret
+    /// </summary>
+    public string ClientSecret { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The TenantId where the client app resides
+    /// </summary>
+    public required string TenantId { get; set; }
+
+    /// <summary>
+    /// Token Audiences
+    /// </summary>
+    public IEnumerable<string> TokenAudiences { get; set; } = Enumerable.Empty<string>();
+
+
+    /// <summary>
+    /// Authority
+    /// </summary>
+    public string Authority
+    {
+        get
+        {
+            return string.Format(CultureInfo.InvariantCulture, Instance, TenantId);
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public string OpenIdMetadataAddress
+    {
+        get
+        {
+            return $"{Authority}/{OpenIdConfigurationPath}";
+        }
+    }
+}
